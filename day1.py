169b1be364a5dc9772bf2b2c701594ad0b92e5ce
maswini-4412 <maswini9267@gmail.com>
2025-08-25 18:39:53 +0530

    message

---
+++ day1.py
@@ -0,0 +1,92 @@
+# Home Work-I (Day 1)
+# 1. Write a program to get the following output. 
+print("Hey there, \nI am full stack developer!")
+
+# 2. Define Python variable, with your won Example?
+x=20
+a="aswini"
+print(a,x)
+
+# 3. Write a Python program to print 'Hello' on screen and then print your name 
+# on a separate line. 
+
+print('hello \n aswini')
+
+# 4. There is also a print() method, which is similar to println().  
+# wont use println fun in python to get new line use end=" " but its avail java only.
+print("hello")
+print("good,morning",end=" ")
+
+# 5. Write a program to get the following output, by using the \t.
+
+print("Raja\tShekhar")
+print("Pavan\tSurya ")
+
+# 6. Write a program for creating a variable with the integer numbers and call 
+# those variables in the print method.
+x=10
+y=20
+z=30
+print("My First Number is:",x)
+print("My second Number is:",y)
+print("My second Number is:",z)
+
+# 7. Write a program for creating a variable as given below: 
+int_java = 90; 
+int_python = 80; 
+int_database = 70; 
+int_WebDevelopment = 60; 
+int_DotNet = 50; 
+
+print("My Java Marks is:",int_java)
+print("My Java Marks is:",int_python)
+print("My Java Marks is:",int_database )
+print("My Java Marks is:",int_WebDevelopment)
+print("My Java Marks is:",int_DotNet)
+
+# 8. Write a program to print the given output? 
+a = 10
+b = 20
+c= 30
+print(a)
+print(b)
+print(c)
+
+# 9. Write a program to print the given output?
+
+print("++++++")
+print("@@@@@@")
+print("******")
+print("######")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
