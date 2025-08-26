72788cefa7926315982ad253dacfa07e051b303a
maswini-4412 <maswini9267@gmail.com>
2025-08-26 11:39:04 +0530

    message

---
+++ day2.py
@@ -0,0 +1,83 @@
+# 1. Calculate the multiplication and sum of two numbers? 
+a=2
+b=2
+print("sum:",a+b)
+print("mul:",a*b)
+
+# 2. Create a string made of the first, middle and last character? 
+inp="QualityThought"
+a=inp[0]
+b=inp[len(inp)//2]
+c=inp[-1]
+result=a+b+c
+print(result)
+
+# One half of 100 is 50. Use a variable to print out the number 50
+a=100
+c=a//2
+print(c)
+
+# 
+c=myTotal = 4 + 2 * 8 - 6
+print(c)
+
+# 
+myTotal = 7 // 5 
+print(myTotal)
+
+#
+myTotal = (8 + 2) * 10 
+print(myTotal)
+
+#
+myTotal = 8 + 2 * 10 
+print(myTotal)
+
+#
+myTotal = 8 + 10 / 2 
+print(myTotal)
+
+#
+myTotal = (8 + 10) / 2 
+print(myTotal)
+
+#
+numberOne=16
+numberTwo=4
+
+answer1= numberOne + numberTwo + 20  
+answer2= 10 * numberOne + (numberTwo + 20)  
+answer3= numberOne + 20 + numberTwo*10
+
+print(answer1,answer2,answer3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
